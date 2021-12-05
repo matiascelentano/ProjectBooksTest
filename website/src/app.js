4ad3977b1ee87ea2f7468f94b609e30f7925489e
matiascelentano <matiascelentano562@gmail.com>
2021-12-05 16:18:31 -0300

    seteando directorio publico para las vistas

--- website/src/app.js
+++ website/src/app.js
@@ -6,19 +6,22 @@ const cookie = require('cookie-parser');
 const path = require('path');
 const method = require('method-override')
 
-//Server configuration
+//Configuracion del Servidor 
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), ()=> console.log('Server running http://localhost:'+ app.get('port')));
+
+//Configuracion de los middlewares
+app.use(express.static(path.resolve(__dirname, '../public')));
 app.use(express.urlencoded({extended:false}))
 app.use(method("_method"))
 app.use(cookie())
 
-// View Engine
+//Seteando View Engine
 app.set("view engine", "ejs");
 app.set("views", path.resolve(__dirname, "./views"));
 
 
 //Rutas
-    //Home
-        const mainRouter = require('./routes/mainRoutes');
-        app.use('/', mainRouter);
+////Home
+    const mainRouter = require('./routes/mainRoutes');
+    app.use('/', mainRouter);
